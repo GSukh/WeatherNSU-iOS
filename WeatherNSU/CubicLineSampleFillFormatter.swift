6a0674d6c9d233ea6271c21ee9023d93ee41c735
Sukhorukov Grigory <grigoriy.sukhorukov@gmail.com>
2017-01-30 00:00:51 +0700

    Initial commit

---
+++ WeatherNSU/CubicLineSampleFillFormatter.swift
@@ -0,0 +1,9 @@
+//
+//  CubicLineSampleFillFormatter.swift
+//  WeatherNSU
+//
+//  Created by Sukhorukov Grigory on 29.01.17.
+//  Copyright © 2017 Sukhorukov Grigory. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherNSU/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  WeatherNSU
+//
+//  Created by Sukhorukov Grigory on 28.01.17.
+//  Copyright © 2017 Sukhorukov Grigory. All rights reserved.
+//
+
+import Foundation
